63b8eacc3ff36ff3c79542a0cf3ac6c9bfe76e78
Javier <javilanger@gmail.com>
2019-03-18 10:46:45 -0300

    Link de logo

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import { Form, Button, ButtonToolbar, ButtonGroup, DropdownButton, Dropdown } from "react-bootstrap";
 import FormControl from "react-bootstrap/FormControl";
 import { connect } from 'react-redux'
-import {setShowModal, setHideModal} from '../store/actions/actions'
+import { setShowModal, setHideModal } from '../store/actions/actions'
 import LogReg from '../containers/logreg'
 
 
@@ -15,35 +15,37 @@ class Header extends React.Component {
 
 
 
-  
-  render() {
-    
-    const userLogin = <ButtonGroup>
-    <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-nested-dropdown">
-    <Dropdown.Item eventKey="1">Profile</Dropdown.Item>
-    <Dropdown.Item eventKey="2">My Purchases</Dropdown.Item>
-  </DropdownButton>
-  </ButtonGroup>
 
-const logReg = <ButtonToolbar>
-<Button
-  variant="primary"
-  onClick={this.props.setShowModal}
->
-  Sign in/sign Up
+  render() {
+
+    const userLogin = <ButtonGroup>
+      <DropdownButton as={ButtonGroup} title="Dropdown" id="bg-nested-dropdown">
+        <Dropdown.Item eventKey="1">Profile</Dropdown.Item>
+        <Dropdown.Item eventKey="2">My Purchases</Dropdown.Item>
+      </DropdownButton>
+    </ButtonGroup>
+
+    const logReg = <ButtonToolbar>
+      <Button
+        variant="primary"
+        onClick={this.props.setShowModal}
+      >
+        Sign in/sign Up
 </Button>
 
-<LogReg />
-</ButtonToolbar>
+      <LogReg />
+    </ButtonToolbar>
 
     return (
       <Navbar bg="dark" expand="lg" >
-        <Navbar.Brand href="/home" className="mr-auto">
+        <Navbar.Brand className="mr-auto">
+        <Link to='/'> 
           <img width="25%" src="./Images/BookStore.png" alt="" />
+        </Link>
         </Navbar.Brand>
         <div>
           <Form inline>
-              {this.props.isLogin ? userLogin : logReg}
+            {this.props.isLogin ? userLogin : logReg}
           </Form>
         </div>
       </Navbar>
@@ -56,14 +58,14 @@ const logReg = <ButtonToolbar>
 
 function mapStateToProps(state) {
   return {
-      showModal: state.showModal,
-      isLogin: state.isLogin
+    showModal: state.showModal,
+    isLogin: state.isLogin
   }
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-      setShowModal: () => dispatch(setShowModal()),
+    setShowModal: () => dispatch(setShowModal()),
   }
 }
 
